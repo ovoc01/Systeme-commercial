9be401ef2890e10976c11d33222ada94ea99c2b1
ovoc01 <spotifymirindra@gmail.com>
2023-11-16 15:14:04 +0300

    initial commit

---
+++ src/main/java/org/syscom/module_achat/ModuleAchatApplication.java
@@ -0,0 +1,13 @@
+package org.syscom.module_achat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleAchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleAchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/syscom/module_achat/ModuleAchatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.syscom.module_achat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleAchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
